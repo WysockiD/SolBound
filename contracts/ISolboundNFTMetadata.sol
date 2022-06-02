101230ac5fef1e3c962c4a4bf064b70db7938d34
Studydwyso <76915554+WysockiD@users.noreply.github.com>
2022-06-02 14:57:13 +0200

    First Commit

---
+++ contracts/ISolboundNFTMetadata.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ISolboundNFT.sol";
+
+interface ISolboundNFTMetadata is ISolboundNFT {
+    /**
+     * @dev Returns the token collection name.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the token collection symbol.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+}
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,5 @@
+const SolboundNFT = artifacts.require("SolboundNFT");
+
+module.exports = function (deployer) {
+  deployer.deploy(SolboundNFT);
+};
